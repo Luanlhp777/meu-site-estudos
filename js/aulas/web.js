9fc57c4b16b995f31227f77250a37c9ede1c4ce1
Luanlhp777 <pereira.luan@gmail.com>
2026-03-09 10:38:01 -0300

    Update aula Programação Web

--- js/aulas/web.js
+++ js/aulas/web.js
@@ -9,6 +9,12 @@ const aulasWeb = [
         titulo: "Programação web",
         conteudo: ` A aula apresentou a introdução ao HTML, explorando a estrutura básica de páginas e o uso de listas, como lista ordenada (ol), lista não ordenada (ul), lista de descrição (dl) e listas aninhadas. Também foi aplicado o uso de links com a tag a para navegação entre páginas ou sites externos.
         Além disso, foi abordado o uso de imagens com caminho relativo, indicando que o atributo href ou src pode apontar tanto para arquivos locais quanto para links externos de um site.. `
-    }
+    },
+    {
+        data: "2026-03-06",
+        titulo: "Programação web",
+        conteudo: ` Na aula de hoje foi apresentado o uso de formulários em HTML e os métodos GET e POST, utilizados para enviar dados. Também aprendemos sobre rótulos (labels), que identificam os campos do formulário, e o uso de placeholder para mostrar um texto de exemplo dentro dos campos de entrada.
+        Além disso, foram utilizados botões do tipo submit, responsáveis por enviar as informações preenchidas no formulário para processamento. `
+    },
 
 ];
