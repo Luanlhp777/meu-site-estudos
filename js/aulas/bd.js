fb4224ca6563aed061f1dfcf7654029d34b1a5c9
Luanlhp777 <pereira.luan@gmail.com>
2026-03-10 08:50:13 -0300

    Atualiza aulas de Banco de Dados e remove matéria Iot

--- js/aulas/bd.js
+++ js/aulas/bd.js
@@ -6,4 +6,16 @@ const aulasBd = [
         No modelo relacional, criado por Edgar F. Codd, os dados são estruturados com base na teoria dos conjuntos. A organização ocorre por meio de tabelas (relações), compostas por linhas (tuplas) e colunas (atributos). Cada atributo possui um domínio, que define o tipo de dado permitido.
         Para evitar duplicidade de registros, cada tabela deve possuir um identificador único chamado chave primária. Além disso, existe a chave estrangeira, responsável por estabelecer o relacionamento entre tabelas, permitindo a extração e associação de dados de diferentes estruturas.`
     },
+    {
+        data: "2026-03-09",
+        titulo: "Banco de dados relacionais",
+        conteudo: `Na aula de hoje estudamos os conceitos de entidade, atributos, relacionamento e cardinalidade dentro da modelagem de banco de dados. Entidade é qualquer objeto ou conceito do mundo real sobre o qual desejamos armazenar informações, como aluno, cliente ou produto. Os atributos são as características que descrevem essas entidades, como nome, endereço, telefone ou idade.
+        Também aprendemos que relacionamento representa a associação entre duas ou mais entidades, geralmente identificado por verbos que ligam os elementos no texto. Já a cardinalidade define a quantidade mínima e máxima de ocorrências entre entidades, indicando relações como um para um ou um para muitos.
+        Durante a aula realizamos dois estudos de caso, onde identificamos entidades, atributos, relacionamentos e cardinalidades a partir de um enunciado. Depois desenhamos os diagramas tanto no caderno quanto utilizando o site app.diagrams.net.
+        `,
+        imagens: [
+            "../../images/imgBd/derLivro.png",
+            "../../images/imgBd/derConsulta.png"
+        ]
+    }
 ];
--- js/aulas/iot.js
+++
@@ -1,8 +0,0 @@
-const aulasIot = [
-    {
-        data: "2026-02-19",
-        titulo: "Sistemas embarcados",
-        conteudo: ` Teste gravando aula. `
-    },
-
-];
