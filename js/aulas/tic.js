0e3a0db858ae2010a5bfd996d68eb789b097cf7c
Luanlhp777 <pereira.luan@gmail.com>
2026-03-25 10:06:36 -0300

    Adiciona aula

--- js/aulas/tic.js
+++ js/aulas/tic.js
@@ -22,5 +22,11 @@ const aulasTic = [
         Ao final da aula, respondemos um questionário sobre Excel para revisar os conteúdos aprendidos e reforçar o entendimento sobre o uso de planilhas e suas funcionalidades.
         `
     },
-
+    {
+        data: "2026-03-23",
+        titulo: "Mala Direta Word",
+        conteudo: `Na aula de hoje aprendemos a utilizar o recurso de mala direta no Microsoft Word, que permite criar documentos personalizados em grande quantidade, como cartas, convites ou comunicados. Foi ensinado como configurar os destinatários, criar novas listas de contatos e personalizar os campos de endereço para cada pessoa.
+        Também vimos como integrar essas informações ao documento principal, possibilitando a geração automática de várias cópias com dados diferentes, tornando o processo mais rápido e organizado. 
+        `
+    },
 ];
